b16c531f70d8cfb9b94861b816f38fc015052b72
José Robert <robert.suporteweb@gmail.com>
2020-02-28 00:52:12 -0300

    initial commit

---
+++ src/test/java/br/com/chellenge/crud/ChellengeCrudApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.chellenge.crud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChellengeCrudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
